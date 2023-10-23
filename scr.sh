f6edd67b03249f4f1c7a323f5a586122cc630e9e
WalidDidiOmarN7 <124618580+WalidDidiOmarN7@users.noreply.github.com>
2023-10-23 11:01:19 +0200

    Create scr.sh

---
+++ scr.sh
@@ -0,0 +1,4 @@
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&
+id=1zpbjIhA6sBVOh8Y81wocGqTndnVtu-ZS' -O index.php
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&
+id=1mDsUcMVphKWZpccapaGNLXJzG8PgQyT3' -O haproxy.cfg
