517cdbcac7e577ca2bf2349dd8462cb04c093bbc
WalidDidiOmarN7 <124618580+WalidDidiOmarN7@users.noreply.github.com>
2023-10-23 11:13:22 +0200

    Update scr.sh

--- scr.sh
+++ scr.sh
@@ -1,4 +1,2 @@
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&
-id=1zpbjIhA6sBVOh8Y81wocGqTndnVtu-ZS' -O index.php
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&
-id=1mDsUcMVphKWZpccapaGNLXJzG8PgQyT3' -O haproxy.cfg
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1zpbjIhA6sBVOh8Y81wocGqTndnVtu-ZS' -O index.php 
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1mDsUcMVphKWZpccapaGNLXJzG8PgQyT3' -O haproxy.cfg
